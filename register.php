76f14ceecb6fb4a103edec28b3170bddbfe6be53
Sarah Mamourou <38317613+ClyviSarahM@users.noreply.github.com>
2018-12-01 23:25:11 +0100

    creating form structure part 1

---
+++ register.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <title>Registration Form</title>
+</head>
+<body>
+    <div class="outterBox">
+        <h2>User Registration</h2>
+        <div class="innerBox">
+            <form action="">
+                <label for="username">Username : </label>
+                <input type="text" name="username" required=""/>
+
+                <label for="firstName">First Name : </label>
+                <input type="text" name="firstName" required=""/>
+            </form>
+        </div>
+    </div>
+</body>
+</html>
